c32909955c0be06fe175a41c9b57ffc5411e41ef
VictorLira <victorliradev@gmail.com>
2021-07-10 18:38:29 -0300

    refactoring

--- main.js
+++ main.js
@@ -1,35 +1,52 @@
-const hamburguer = document.getElementById('hamburguer');
-const navUL = document.getElementById('nav-ul');
+const hamburguer = document.getElementById("hamburguer");
+const navUL = document.getElementById("nav-ul");
+const banner = document.querySelector(".banner");
+const header = document.querySelector("header");
+const navigation = document.querySelector(".nav__links");
+const allSections = document.querySelectorAll(".section");
 
-hamburguer.addEventListener('click', () =>{
-    navUL.classList.toggle('show');
-})
+hamburguer.addEventListener("click", () => {
+    navUL.classList.toggle("show");
+});
 
-//smooth scroth 
-const navigation = document.querySelector('.nav__links');
-
-navigation.addEventListener('click', function (e) {
+//smooth scroth
+navigation.addEventListener("click", function (e) {
     e.preventDefault();
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
-})
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
 
 //revealing sections on scroll
-const allSections = document.querySelectorAll('.section');
-
 const removeHiddenClass = function (entries, observer) {
     const [entry] = entries;
     if (!entry.isIntersecting) return;
-    entry.target.classList.remove('section--hidden');
+    entry.target.classList.remove("section--hidden");
     observer.unobserve(entry.target);
-}
+};
 
 const sectionObserver = new IntersectionObserver(removeHiddenClass, {
     root: null,
-    threshold: 0.15
+    threshold: 0.1,
 });
 
-allSections.forEach(section => {
+allSections.forEach((section) => {
     sectionObserver.observe(section);
-    section.classList.add('section--hidden')
-})
+    section.classList.add("section--hidden");
+});
+
+//sticky menu
+const addStickClass = function (entries, observer) {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+        header.classList.remove("sticky");
+    }
+    if (!entry.isIntersecting) {
+        header.classList.add("sticky");
+    }
+};
+
+const bannerOberver = new IntersectionObserver(addStickClass, {
+    root: null,
+    threshold: 0,
+});
+bannerOberver.observe(banner);
