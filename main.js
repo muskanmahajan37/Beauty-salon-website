76537058ac4acf85bdf3f29631f6f9f376905339
VictorLira <victorliradev@gmail.com>
2021-07-10 11:57:11 -0300

    smooth scroll

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ const navUL = document.getElementById('nav-ul');
 hamburguer.addEventListener('click', () =>{
     navUL.classList.toggle('show');
 })
+
+//smooth scroth 
+const navigation = document.querySelector('.nav__links');
+
+navigation.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    console.log(id)
+})
