e28b6331aa80e42e18484a20fa112653a9664090
VictorLira <victorliradev@gmail.com>
2021-07-10 14:04:25 -0300

    revealing elements on scroll

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ navigation.addEventListener('click', function (e) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
 })
+
+//revealing sections on scroll
+const allSections = document.querySelectorAll('.section');
+
+const removeHiddenClass = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(removeHiddenClass, {
+    root: null,
+    threshold: 0.15
+});
+
+allSections.forEach(section => {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden')
+})
